ba3c9187541b37c9ac249ff9b92a5430ba7baffd
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 12:43:02 -0400

    Update `MixProject` dependency list [`:boundary`]

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Cavalree.MixProject do
   def project() do
     [
       app: :cavalree,
-      deps: [],
+      deps: [{:boundary, "~> 0.10", runtime: false}],
       deps_path: "dep",
       elixir: "~> 1.20",
       elixirc_options: [warnings_as_errors: true],
