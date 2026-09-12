ca304232e3cbb298c3cb70c92a756fddea736048
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 13:01:28 -0400

    Create `Credo` configuration [`default`]

--- mix.exs
+++ mix.exs
@@ -31,7 +31,8 @@ defmodule Cavalree.MixProject do
           "boundary.ex_doc_groups",
           &write_moduledoc_group/1,
           "format .boundary.exs"
-        ]
+        ],
+        credo: "credo --config-name default"
       ],
       app: :cavalree,
       boundary: [default: [type: :strict]],
