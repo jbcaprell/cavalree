a10ee2787d912b4b54e376c94b429701de780e7d
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 20:52:33 -0400

    Create `Type.Enum` function [`admits?/2`]

--- lib/masonree/type/enum.ex
+++ lib/masonree/type/enum.ex
@@ -1,4 +1,14 @@
 defmodule Masonree.Type.Enum do
   @moduledoc "Defines the `{:enum, values}` member of the lattice."
   @moduledoc since: "0.3.0"
+
+  @behaviour Masonree.Type
+
+  alias Masonree
+
+  alias Masonree.Type
+
+  @impl Type
+  def admits?(payload, value) when is_list(payload), do: value in payload
+  def admits?(_payload, _value), do: false
 end
