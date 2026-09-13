2ddc68f7c5eee22438ddae7ba005790beca52319
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 21:04:08 -0400

    Update `Type` function [`admits?/2`]

--- lib/masonree/type.ex
+++ lib/masonree/type.ex
@@ -33,7 +33,7 @@ defmodule Masonree.Type do
 
   @typedoc "Represents a type, with the payload where its member takes one."
   @typedoc since: "0.3.0"
-  @type t() :: :boolean | {:enum, [value()]}
+  @type t() :: :boolean | :number | {:enum, [value()]}
 
   @typedoc "Represents the value a member is asked about."
   @typedoc since: "0.3.0"
@@ -43,7 +43,7 @@ defmodule Masonree.Type do
   @doc since: "0.3.0"
   @callback admits?(payload(), value()) :: boolean()
 
-  @modules %{boolean: Type.Boolean, enum: Type.Enum}
+  @modules %{boolean: Type.Boolean, enum: Type.Enum, number: Type.Number}
 
   @doc """
   Returns whether `type` may hold `value`.
