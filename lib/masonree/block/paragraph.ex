75f2891391bf0e27bfaea17362c3e7d104ee2f42
Jonathan Caprell <jonathan@caprell.com>
2026-09-13 09:19:02 -0400

    Create `Block.Paragraph` module

---
+++ lib/masonree/block/paragraph.ex
@@ -0,0 +1,4 @@
+defmodule Masonree.Block.Paragraph do
+  @moduledoc "Defines a block."
+  @moduledoc since: "0.3.0"
+end
---
+++ test/masonree/block/paragraph_test.exs
@@ -0,0 +1,12 @@
+defmodule Masonree.Block.ParagraphTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.3.0"
+
+  use ExUnit.Case, async: true
+
+  alias Masonree
+
+  alias Masonree.Block
+
+  doctest Block.Paragraph, import: true
+end
