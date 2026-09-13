c4d6626aa3b7d4b38c7f474accec7fd10dbdbd5e
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 20:47:34 -0400

    Create `Type.Boolean` module

---
+++ lib/masonree/type/boolean.ex
@@ -0,0 +1,4 @@
+defmodule Masonree.Type.Boolean do
+  @moduledoc "Defines the `:boolean` member of the lattice."
+  @moduledoc since: "0.3.0"
+end
---
+++ test/masonree/type/boolean_test.exs
@@ -0,0 +1,12 @@
+defmodule Masonree.Type.BooleanTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.3.0"
+
+  use ExUnit.Case, async: true
+
+  alias Masonree
+
+  alias Masonree.Type
+
+  doctest Type.Boolean, import: true
+end
