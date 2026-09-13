37d26f68750371761d9211a58919208fe66e8672
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 21:36:07 -0400

    Create `Node` module

---
+++ lib/masonree/node.ex
@@ -0,0 +1,9 @@
+defmodule Masonree.Node do
+  @moduledoc """
+  Defines one instance of a block in a document.
+
+  A document is a tree of nodes and nothing else, which is what lets the tree be
+  the only thing in a database.
+  """
+  @moduledoc since: "0.3.0"
+end
---
+++ test/masonree/node_test.exs
@@ -0,0 +1,12 @@
+defmodule Masonree.NodeTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.3.0"
+
+  use ExUnit.Case, async: true
+
+  alias Masonree
+
+  alias Masonree.Node
+
+  doctest Node, import: true
+end
