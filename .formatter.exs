cfd127d34d050e206acb03848ae1058d835c56d2
Jonathan Caprell <jonathan@caprell.com>
2026-09-13 12:31:03 -0400

    Update `MixProject` project [`:elixirc_paths`]

--- .formatter.exs
+++ .formatter.exs
@@ -2,7 +2,7 @@
   import_deps: [:ecto],
   inputs: [
     ".{boundary,credo,dialyzer,formatter}.exs",
-    "{lib,test}/**/*.{ex,exs}",
+    "{lib,support,test}/**/*.{ex,exs}",
     "mix.{exs,lock}"
   ],
   line_length: 80,
