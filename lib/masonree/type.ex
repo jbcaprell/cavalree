89df3558f901a71d26e8c766ca08395fb9058f17
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 20:43:46 -0400

    Create `Type` module

---
+++ lib/masonree/type.ex
@@ -0,0 +1,4 @@
+defmodule Masonree.Type do
+  @moduledoc "Defines the lattice an attribute’s type is drawn from."
+  @moduledoc since: "0.3.0"
+end
---
+++ test/masonree/type_test.exs
@@ -0,0 +1,12 @@
+defmodule Masonree.TypeTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.3.0"
+
+  use ExUnit.Case, async: true
+
+  alias Masonree
+
+  alias Masonree.Type
+
+  doctest Type, import: true
+end
