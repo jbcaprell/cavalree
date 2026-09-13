e14f138c107d90d4d424cfb487f46eb27ab48b6b
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 21:00:59 -0400

    Create `Type.Number` module

---
+++ lib/masonree/type/number.ex
@@ -0,0 +1,4 @@
+defmodule Masonree.Type.Number do
+  @moduledoc "Defines the `:number` member of the lattice."
+  @moduledoc since: "0.3.0"
+end
---
+++ test/masonree/type/number_test.exs
@@ -0,0 +1,12 @@
+defmodule Masonree.Type.NumberTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.3.0"
+
+  use ExUnit.Case, async: true
+
+  alias Masonree
+
+  alias Masonree.Type
+
+  doctest Type.Number, import: true
+end
