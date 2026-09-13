ca4c936757918b977c022f6a8de1a9b1fdc948da
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 21:29:49 -0400

    Create `Manifest` struct

--- test/masonree/manifest_test.exs
+++ test/masonree/manifest_test.exs
@@ -9,4 +9,20 @@ defmodule Masonree.ManifestTest do
   alias Masonree.Manifest
 
   doctest Manifest, import: true
+
+  describe "%Manifest{}" do
+    test "defaults every field it does not enforce to nothing" do
+      manifest = %Manifest{name: "test/example", version: 1}
+
+      for %{field: field, required: false} <- Manifest.__info__(:struct) do
+        assert Map.fetch!(manifest, field) in [%{}, [], nil]
+      end
+    end
+
+    test "enforces a name and a version" do
+      message = ~r"must also be given .*: \[:name, :version\]"
+
+      assert_raise ArgumentError, message, fn -> struct!(Manifest, []) end
+    end
+  end
 end
