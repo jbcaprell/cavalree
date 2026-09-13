8855c8a4848e582aed69431649a84bf199b113c2
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 20:44:48 -0400

    Create `Type` callback [`c:admits?/2`]

--- test/masonree/type_test.exs
+++ test/masonree/type_test.exs
@@ -9,4 +9,11 @@ defmodule Masonree.TypeTest do
   alias Masonree.Type
 
   doctest Type, import: true
+
+  describe "Type" do
+    test "the lattice asks one question, and every member must answer" do
+      assert Type.behaviour_info(:callbacks) == [admits?: 2]
+      assert Type.behaviour_info(:optional_callbacks) == []
+    end
+  end
 end
