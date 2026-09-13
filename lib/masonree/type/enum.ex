5f063f3adf78facd4a49e19cf993952a5dab0f76
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 20:50:57 -0400

    Create `Type.Enum` module

---
+++ lib/masonree/type/enum.ex
@@ -0,0 +1,4 @@
+defmodule Masonree.Type.Enum do
+  @moduledoc "Defines the `{:enum, values}` member of the lattice."
+  @moduledoc since: "0.3.0"
+end
---
+++ test/masonree/type/enum_test.exs
@@ -0,0 +1,12 @@
+defmodule Masonree.Type.EnumTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.3.0"
+
+  use ExUnit.Case, async: true
+
+  alias Masonree
+
+  alias Masonree.Type
+
+  doctest Type.Enum, import: true
+end
