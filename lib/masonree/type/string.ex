c51200e82f531e4cc475019a50bf8e4d0bf2fa94
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 21:05:13 -0400

    Create `Type.String` member

---
+++ lib/masonree/type/string.ex
@@ -0,0 +1,4 @@
+defmodule Masonree.Type.String do
+  @moduledoc "Defines the `:string` member of the lattice."
+  @moduledoc since: "0.3.0"
+end
---
+++ test/masonree/type/string_test.exs
@@ -0,0 +1,12 @@
+defmodule Masonree.Type.StringTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.3.0"
+
+  use ExUnit.Case, async: true
+
+  alias Masonree
+
+  alias Masonree.Type
+
+  doctest Type.String, import: true
+end
