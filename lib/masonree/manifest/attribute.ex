5b45bc08cf8a29aea93b2606db1abac0db03ff71
Jonathan Caprell <jonathan@caprell.com>
2026-09-12 21:32:40 -0400

    Create `Attribute` module

---
+++ lib/masonree/manifest/attribute.ex
@@ -0,0 +1,10 @@
+defmodule Masonree.Manifest.Attribute do
+  @moduledoc """
+  Defines one attribute of a block.
+
+  An attribute is the only part of a manifest that fixes the meaning of stored
+  values: a node's attributes hold values whose meaning is declared here, and
+  the rest of a manifest is code.
+  """
+  @moduledoc since: "0.3.0"
+end
---
+++ test/masonree/manifest/attribute_test.exs
@@ -0,0 +1,14 @@
+defmodule Masonree.Manifest.AttributeTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.3.0"
+
+  use ExUnit.Case, async: true
+
+  alias Masonree
+
+  alias Masonree.Manifest
+
+  alias Manifest.Attribute
+
+  doctest Attribute, import: true
+end
