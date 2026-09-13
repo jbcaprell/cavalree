4edc8ff98dc1ab69c2e0758e4559034b6358f857
Jonathan Caprell <jonathan@caprell.com>
2026-09-13 07:21:41 -0400

    Create `Document` module

---
+++ lib/masonree/document.ex
@@ -0,0 +1,9 @@
+defmodule Masonree.Document do
+  @moduledoc """
+  Defines a page as an ordered list of nodes.
+
+  A document is a tree of `Masonree.Node` structs and nothing else, which is
+  what lets the tree be the only thing in a database.
+  """
+  @moduledoc since: "0.3.0"
+end
---
+++ test/masonree/document_test.exs
@@ -0,0 +1,12 @@
+defmodule Masonree.DocumentTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.3.0"
+
+  use ExUnit.Case, async: true
+
+  alias Masonree
+
+  alias Masonree.Document
+
+  doctest Document, import: true
+end
