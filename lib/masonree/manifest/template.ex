0c08ebf51f583dd5ce04813dd23d65788b91cf61
Jonathan Caprell <jonathan@caprell.com>
2026-09-13 20:49:06 -0400

    Create `Template` module

---
+++ lib/masonree/manifest/template.ex
@@ -0,0 +1,4 @@
+defmodule Masonree.Manifest.Template do
+  @moduledoc "Defines the tree a block’s interior starts from."
+  @moduledoc since: "0.6.0"
+end
---
+++ test/masonree/manifest/template_test.exs
@@ -0,0 +1,14 @@
+defmodule Masonree.Manifest.TemplateTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.6.0"
+
+  use ExUnit.Case, async: true
+
+  alias Masonree
+
+  alias Masonree.Manifest
+
+  alias Manifest.Template
+
+  doctest Template, import: true
+end
