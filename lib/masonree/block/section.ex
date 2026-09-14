5297c1a8fa743b0c189a97d5fbb8f315b0c8c0d9
Jonathan Caprell <jonathan@caprell.com>
2026-09-13 21:40:18 -0400

    Create `Block.Section` module

---
+++ lib/masonree/block/section.ex
@@ -0,0 +1,4 @@
+defmodule Masonree.Block.Section do
+  @moduledoc "Defines a block."
+  @moduledoc since: "0.6.0"
+end
---
+++ test/masonree/block/section_test.exs
@@ -0,0 +1,12 @@
+defmodule Masonree.Block.SectionTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.6.0"
+
+  use ExUnit.Case, async: true
+
+  alias Masonree
+
+  alias Masonree.Block
+
+  doctest Block.Section, import: true
+end
