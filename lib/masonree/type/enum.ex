a6d6ac3f1b218cd4e5ed9c94aae405e78047f734
Jonathan Caprell <jonathan@caprell.com>
2026-09-14 19:22:45 -0400

    Create `Type.Enum` function [`heal/3`]

--- lib/masonree/type/enum.ex
+++ lib/masonree/type/enum.ex
@@ -8,6 +8,16 @@ defmodule Masonree.Type.Enum do
 
   alias Masonree.Type
 
+  @typep default() :: Type.default()
+  @typep healing() :: Type.healing()
+  @typep payload() :: Type.payload()
+  @typep value() :: Type.value()
+
+  # @impl Type
+  @doc "Returns the repair of `value` toward `default`, given `payload`."
+  @spec heal(payload(), value(), default()) :: healing()
+  def heal(_payload, _value, default), do: {:coerced, default}
+
   @impl Type
   def admits?(payload, value) when is_list(payload), do: value in payload
   def admits?(_payload, _value), do: false
