72fc796b9974a39e134fb702d2cb356ae475f518
Jonathan Caprell <jonathan@caprell.com>
2026-09-14 08:26:18 -0400

    Create `v0.7.0` release

--- mix.exs
+++ mix.exs
@@ -78,7 +78,7 @@ defmodule Cavalree.MixProject do
       name: "Cavalree",
       start_permanent: env == :prod,
       test_coverage: [ignore_modules: [~r/\AMasonreeBench(\.|\z)/]],
-      version: "0.6.1"
+      version: "0.7.0"
     ]
   end
 
