d8bfd63e8b4f4bef6abd951473d943baf42b4c44
Jonathan Caprell <jonathan@caprell.com>
2026-09-13 20:51:53 -0400

    Create `Containment` module

---
+++ lib/masonree/manifest/containment.ex
@@ -0,0 +1,4 @@
+defmodule Masonree.Manifest.Containment do
+  @moduledoc "Defines a block’s rule about its interior."
+  @moduledoc since: "0.6.0"
+end
---
+++ test/masonree/manifest/containment_test.exs
@@ -0,0 +1,14 @@
+defmodule Masonree.Manifest.ContainmentTest do
+  @moduledoc "Defines an `ExUnit.Case` case."
+  @moduledoc since: "0.6.0"
+
+  use ExUnit.Case, async: true
+
+  alias Masonree
+
+  alias Masonree.Manifest
+
+  alias Manifest.Containment
+
+  doctest Containment, import: true
+end
